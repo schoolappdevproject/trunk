e1ede4b7ac38d2d7a33f0e12398013818849d592
subhadip <subhadiplayek@gmail.com>
2016-04-07 16:44:16 +0530

    query comments removed

--- api/new_queries.sql
+++ api/new_queries.sql
@@ -1,4 +1,3 @@
---- Foreign key queries ---
 
 ALTER TABLE `tbl_blog_data` ADD FOREIGN KEY ( `id_user_data` ) REFERENCES `school_db`.`tbl_users` (
 `user_id`
@@ -12,8 +11,6 @@ ALTER TABLE `tbl_blog_data` ADD FOREIGN KEY ( `id_school` ) REFERENCES `school_d
 `school_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
 
------------------------------
-
 ALTER TABLE `picture` ADD FOREIGN KEY ( `id_school` ) REFERENCES `school_db`.`tbl_school_main_table` (
 `school_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
@@ -21,8 +18,6 @@ ALTER TABLE `picture` ADD FOREIGN KEY ( `id_school` ) REFERENCES `school_db`.`tb
 ALTER TABLE `picture` ADD FOREIGN KEY ( `id_user` ) REFERENCES `school_db`.`tbl_users` (
 `user_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
-
-------------------------------
 
 ALTER TABLE `tbl_comments_table` ADD FOREIGN KEY ( `id_blog` ) REFERENCES `school_db`.`tbl_blog_data` (
 `id_blog_data`
@@ -32,8 +27,6 @@ ALTER TABLE `tbl_comments_table` ADD FOREIGN KEY ( `user_id` ) REFERENCES `schoo
 `user_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
 
---------------------------------
-
 ALTER TABLE `tbl_likes_blogs` ADD FOREIGN KEY ( `id_blog` ) REFERENCES `school_db`.`tbl_blog_data` (
 `id_blog_data`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
@@ -41,8 +34,6 @@ ALTER TABLE `tbl_likes_blogs` ADD FOREIGN KEY ( `id_blog` ) REFERENCES `school_d
 ALTER TABLE `tbl_likes_blogs` ADD FOREIGN KEY ( `id_user` ) REFERENCES `school_db`.`tbl_users` (
 `user_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
-
------------------------------------
 
 ALTER TABLE `tbl_likes_reviews` ADD FOREIGN KEY ( `id_review` ) REFERENCES `school_db`.`tbl_review_data` (
 `id_review_data`
@@ -52,13 +43,9 @@ ALTER TABLE `tbl_likes_reviews` ADD FOREIGN KEY ( `id_users` ) REFERENCES `schoo
 `user_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
 
-----------------------------
-
 ALTER TABLE `tbl_like_comments` ADD FOREIGN KEY ( `id_user` ) REFERENCES `school_db`.`tbl_users` (
 `user_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
-
-------------------------------
 
 ALTER TABLE `tbl_permission_table` ADD FOREIGN KEY ( `admin_id` ) REFERENCES `school_db`.`tbl_admin` (
 `admin_id`
@@ -68,8 +55,6 @@ ALTER TABLE `tbl_permission_table` ADD FOREIGN KEY ( `school_id` ) REFERENCES `s
 `school_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
 
------------------------------------
-
 ALTER TABLE `tbl_ratings_table` ADD FOREIGN KEY ( `id_school` ) REFERENCES `school_db`.`tbl_school_main_table` (
 `school_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
@@ -77,8 +62,6 @@ ALTER TABLE `tbl_ratings_table` ADD FOREIGN KEY ( `id_school` ) REFERENCES `scho
 ALTER TABLE `tbl_ratings_table` ADD FOREIGN KEY ( `user_id` ) REFERENCES `school_db`.`tbl_users` (
 `user_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
-
----------------------------------------
 
 ALTER TABLE `tbl_review_data` CHANGE `id_review_attachment` `id_review_attachment` INT( 11 ) NOT NULL ;
 
@@ -94,8 +77,6 @@ ALTER TABLE `tbl_review_data` ADD FOREIGN KEY ( `id_rating_data` ) REFERENCES `s
 `id_rating`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
 
---------------------------------
-
 ALTER TABLE `tbl_student_data` ADD FOREIGN KEY ( `tbl_user_id` ) REFERENCES `school_db`.`tbl_users` (
 `user_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
@@ -104,8 +85,6 @@ ALTER TABLE `tbl_student_data` ADD FOREIGN KEY ( `tbl_school_id` ) REFERENCES `s
 `school_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
 
-------------------------------------
-
 ALTER TABLE `video` ADD FOREIGN KEY ( `id_school` ) REFERENCES `school_db`.`tbl_school_main_table` (
 `school_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
@@ -113,5 +92,3 @@ ALTER TABLE `video` ADD FOREIGN KEY ( `id_school` ) REFERENCES `school_db`.`tbl_
 ALTER TABLE `video` ADD FOREIGN KEY ( `id_user` ) REFERENCES `school_db`.`tbl_users` (
 `user_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
-
-----------------------------------
