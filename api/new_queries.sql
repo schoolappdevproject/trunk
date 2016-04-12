0fd0b001caa8c3997380d5a663e015a22df9eec4
subhadip <subhadiplayek@gmail.com>
2016-04-12 11:23:35 +0530

    Google user table added | minor changes

--- api/new_queries.sql
+++ api/new_queries.sql
@@ -92,3 +92,25 @@ ALTER TABLE `video` ADD FOREIGN KEY ( `id_school` ) REFERENCES `school_db`.`tbl_
 ALTER TABLE `video` ADD FOREIGN KEY ( `id_user` ) REFERENCES `school_db`.`tbl_users` (
 `user_id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
+
+
+RENAME TABLE `school_db`.`tbl_comments_table` TO `school_db`.`tbl_comments` ;
+
+RENAME TABLE `school_db`.`tbl_permission_table` TO `school_db`.`tbl_permission` ;
+
+RENAME TABLE `school_db`.`tbl_school_main_table` TO `school_db`.`tbl_school_main` ;
+
+RENAME TABLE `school_db`.`tbl_ratings_table` TO `school_db`.`tbl_ratings` ;
+
+RENAME TABLE `school_db`.`video` TO `school_db`.`tbl_video` ;
+
+RENAME TABLE `school_db`.`picture` TO `school_db`.`tbl_picture` ;
+
+CREATE TABLE IF NOT EXISTS `tbl_google_users` (
+  `google_id` decimal(21,0) NOT NULL,
+  `google_name` varchar(60) NOT NULL,
+  `google_email` varchar(60) NOT NULL,
+  `google_link` varchar(60) NOT NULL,
+  `google_picture_link` varchar(200) NOT NULL,
+  PRIMARY KEY (`google_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
