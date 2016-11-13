3897b53d3c3046790e65a134855e3409c23b58fd
abhijit roy <gta4roy@gmail.com>
2016-11-13 21:41:02 +0530

    new file:   api/load_data/load_school_average_rating.php 	new file:   api/test/test_avg_rating.py 	modified:   ui/school_overview.html
    	modified:   api/trace/log/trace.log
    	deleted:    ui/main.html

---
+++ api/load_data/load_school_average_rating.php
@@ -0,0 +1,42 @@
+
+<?php
+
+/*
+API is used to save the reviews 
+*/
+include_once '../common/connection.php';
+require("../trace/MyLogPHP-1.2.1.class.php");
+$method = $_SERVER['REQUEST_METHOD'];
+$log = new MyLogPHP();
+if ($method == 'POST') {
+
+    
+      $school_id    = trim($_POST['school_id']);
+    
+    if(empty($school_id))
+    {
+       $log->info("HTTP/1.1 400 Bad Request");
+        header("HTTP/1.1 400 Bad Request");    
+    }
+    else
+    {
+
+	$qry = "select ".
+            "avg(tbl_ratings_table.rating_points) as avg_rating ".
+            "from tbl_ratings_table where ".
+            "tbl_ratings_table.id_school = $school_id";
+     
+       
+      $result = $database->query($qry)->fetchAll();
+      echo json_encode($result);
+        
+    }
+      
+    
+} 
+else
+{
+    $log->info("HTTP/1.1 400 Bad Request");
+     header("HTTP/1.1 400 Bad Request");
+}
+?>
